55c1d41b6855664f15312b7a05d109cb71116aa7
dlarrota <dlarrota@hugeinc.com>
2018-02-05 22:43:22 -0500

    data request from API

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
 
-const Home = () => {
-    return (
-        <main>
-            <header>
-                <h1>The Simpsons</h1>
-            </header>
-        </main>
-    )
+class Home extends React.Component {
+    componentDidMount() {
+        axios.get('https://simpsons-api.herokuapp.com/characters')
+        .then(response => {
+            console.log(response.dataº)
+            return response.data
+        })
+        .catch(error => {
+            throw new Error(error)
+        })
+    }
+    render () {
+        return (
+            <main>
+                <header>
+                    <h1>The Simpsons</h1>
+                </header>
+            </main>
+        )
+    }
+}
+/*
+    Home propTypes
+*/
+Home.propTypes = {
+    // : PropTypes.
 }
 
 export default Home
