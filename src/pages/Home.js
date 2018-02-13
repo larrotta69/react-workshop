a25eeec3fb15fe3b15a566fcf2a330ebc6a21de1
dlarrota <dlarrota@hugeinc.com>
2018-02-13 17:56:23 -0500

    High Order Components support

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {compose} from 'recompose'
 
 import Board from '../containers/Board'
-import Layout from '../components/Layout'
+
+import {withLayout} from '../HOC'
 
 const Home = (props) => {
     const {params: {name}} = props.match
     return (
-        <Layout>
-            <Board characterMain={name}/>
-        </Layout>
+        <Board characterMain={name}/>
     )
 }
 Home.propTypes = {
     match: PropTypes.object
 }
 
-export default Home
+export default compose(
+    withLayout
+)(Home)
